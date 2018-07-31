fb09a0bdda43432b46c333f0a2d508750ef030f1
BowenWang29 <wangbowen1992@hotmail.com>
2018-07-31 11:56:30 +0000

    changed smartaqnethome setting

--- sbin/startFrost.sh
+++ sbin/startFrost.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd ..
 if test -z "$smartaqnethome"
-then echo "export smartaqnethome=localhost" > /etc/environment
+then echo "export smartaqnethome=$(uname -n).teco.edu" > /etc/environment
 fi
 source /etc/environment
 # Set up SensorThingsServerDocker
